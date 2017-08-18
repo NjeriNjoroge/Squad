63aa6b23ab0d97e1861feac9b2d43746af268870
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-08-18 16:04:08 +0300

    users can now add multiple heroes and view them on the index page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -35,7 +35,7 @@ post("/hero", (request, response) -> {
 //displaying hero on form template
 get("/", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
-  model.put("hero", request.session().attribute("hero"));
+  model.put("heroes", request.session().attribute("heroes"));
   model.put("template", "templates/form.vtl");
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
