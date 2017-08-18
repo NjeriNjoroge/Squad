697b38bde17fc5a03cc244a4cc30ef7e5b0911bc
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-08-18 15:32:18 +0300

    created an array in App.java for it to display multiple heroes, currently cannot

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,35 +3,36 @@ import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-//route for showing the form
-// get("/", (request, response) -> {
-//   Map<String, Object> model = new HashMap<String, Object>();
-//   model.put("template", "templates/form.vtl");
-//   return new ModelAndView(model, layout);
-// }, new VelocityTemplateEngine());
-
 //picking values from the form.vtl
 post("/hero", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
+
+  ArrayList<Hero> heroes = request.session().attribute("heroes");
+  if (heroes == null) {
+      heroes = new ArrayList<Hero>();
+      request.session().attribute("heroes", heroes);
+  }
 
   String createdName = request.queryParams("name");
   int createdAge = Integer.parseInt(request.queryParams("age"));
   String createdSpecialPower = request.queryParams("specialPower");
   String createdWeakness = request.queryParams("weakness");
   Hero newHero = new Hero(createdName, createdAge, createdSpecialPower, createdWeakness);
-  request.session().attribute("hero", newHero);
+  heroes.add(newHero);
+  //request.session().attribute("hero", newHero);
 
   model.put("template", "templates/success.vtl");
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
-//displaying task on index template
+//displaying hero on form template
 get("/", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
   model.put("hero", request.session().attribute("hero"));
