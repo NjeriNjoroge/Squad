7996f95b28f63d737e953facf8d237869237e7be
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-08-19 11:08:40 +0300

    applied RESTFUL routing to the hero

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,31 +10,59 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+//displaying hero on form template
+// get("/", (request, response) -> {
+//   Map<String, Object> model = new HashMap<String, Object>();
+//   model.put("heroes", request.session().attribute("heroes"));
+//   model.put("template", "templates/form.vtl");
+//   return new ModelAndView(model, layout);
+// }, new VelocityTemplateEngine());
+
+//displaying a welcome message
+get("/", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  model.put("template", "templates/index.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
+//display form
+get("/hero", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  model.put("template", "templates/form.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
 //picking values from the form.vtl
 post("/hero", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
 
-  ArrayList<Hero> heroes = request.session().attribute("heroes");
-  if (heroes == null) {
-      heroes = new ArrayList<Hero>();
-      request.session().attribute("heroes", heroes);
-  }
+  // ArrayList<Hero> heroes = request.session().attribute("heroes");
+  // if (heroes == null) {
+  //     heroes = new ArrayList<Hero>();
+  //     request.session().attribute("heroes", heroes);
+  // }
 
   String createdName = request.queryParams("name");
   int createdAge = Integer.parseInt(request.queryParams("age"));
   String createdSpecialPower = request.queryParams("specialPower");
   String createdWeakness = request.queryParams("weakness");
   Hero newHero = new Hero(createdName, createdAge, createdSpecialPower, createdWeakness);
-  heroes.add(newHero);
+  //heroes.add(newHero);
 
   model.put("template", "templates/success.vtl");
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
-//displaying hero on form template
-get("/", (request, response) -> {
+//user requests page, Velocity loops through objects and displays them
+get("/heroes", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
-  model.put("heroes", request.session().attribute("heroes"));
+  model.put("heroes", Hero.all());
+  model.put("template", "templates/heroes.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
+get("/hero/new", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
   model.put("template", "templates/form.vtl");
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
